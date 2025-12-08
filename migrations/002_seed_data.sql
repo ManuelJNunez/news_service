b3daaafcc08f45a284d76d55c755edafa56699a6
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2025-12-08 16:41:57 +0100

    Remove useless quote

--- migrations/002_seed_data.sql
+++ migrations/002_seed_data.sql
@@ -1,5 +1,5 @@
 INSERT INTO News (title, body, datetime) VALUES
-    ('Lorem ipsum', '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', NOW() - INTERVAL '5 days'),
+    ('Lorem ipsum', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', NOW() - INTERVAL '5 days'),
     ('Wannacry: el ransomware que alertó a todo el mundo', 'Wannacry afectó a varios sistemas informáticos de un montón de empresa en septiembre de 2018.', NOW() - INTERVAL '2 days'),
     ('Go 1.25 Released', 'Most of its changes are in the implementation of the toolchain, runtime, and libraries.', NOW() - INTERVAL '1 day'),
     ('Listado de empresas afectadas por vulnerabilidades SQLi', 'Hoy vamos a ver un gran listado de casos reales de empresas que fueron afectada por esta vulnerabilidad tan popular.', NOW()),
