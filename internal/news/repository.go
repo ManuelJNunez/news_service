ab980ac964b25dc2f1aa8d306ec6065c617dc8e3
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2025-12-09 16:27:17 +0100

    Harden security against regular SQLi

--- internal/news/repository.go
+++ internal/news/repository.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"log/slog"
+	"strings"
 )
 
 // ErrNotFound is used when it is not possible to find the requested News.
@@ -25,6 +26,13 @@ func NewRepository(db *sql.DB) Repository {
 
 func (s *postgresRepository) GetByID(ctx context.Context, id string) (*Article, error) {
 	slog.Debug("fetching news", slog.String("id", id))
+
+	// Block dangerous SQL keywords
+	if containsSQLKeywords(id) {
+		slog.Warn("blocked SQLi attempt", slog.String("id", id))
+		return nil, ErrNewsNotFound
+	}
+
 	query := fmt.Sprintf("SELECT title, body, datetime FROM news WHERE id=%s;", id)
 
 	// Get a single row from the database (the first one) and copy the fetched data into the Article struct
@@ -46,4 +54,16 @@ func (s *postgresRepository) GetByID(ctx context.Context, id string) (*Article, 
 
 	slog.Info("successfully fetched news", slog.String("id", id))
 	return &n, nil
+}
+
+func containsSQLKeywords(input string) bool {
+	dangerousKeywords := []string{"INSERT", "UPDATE", "DELETE", "DROP", "CREATE", "ALTER", "ORDER", "GROUP", ";", "/*", "*/", "--"}
+	inputUpper := strings.ToUpper(input)
+
+	for _, keyword := range dangerousKeywords {
+		if strings.Contains(inputUpper, keyword) {
+			return true
+		}
+	}
+	return false
 }
